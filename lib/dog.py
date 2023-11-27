26634e90322c5f7dcab88e13f32930556988e17c
Emmanue <kioxmanu7@gmail.com>
2023-11-27 12:04:52 +0300

    assignment completed

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
+# dog.py
+
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+    def sit(self):
+        print("The dog is sitting.")
+
+    def bark(self):
+        print("Woof!")
